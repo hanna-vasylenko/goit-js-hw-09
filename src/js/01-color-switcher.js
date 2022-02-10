7a706c0fba9a1569ba4712aaee7a359d0d661f58
Hanna Vasylenko <hanna.vasylenko05@gmail.com>
2022-02-10 15:51:14 +0200

    Add first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,5 +3,23 @@ const refs = {
   btnStop: document.querySelector('[data-stop]'),
 };
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let intervalId = null;
+const onStart = () => {
+  refs.btnStart.setAttribute('disabled', 'disable');
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+};
+
+const onStop = () => {
+  refs.btnStart.removeAttribute('disabled');
+  clearInterval(intervalId);
+  intervalId = null;
+};
+
 refs.btnStart.addEventListener('click', onStart);
 refs.btnStop.addEventListener('click', onStop);
