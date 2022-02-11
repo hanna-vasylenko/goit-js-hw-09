ab25bf35485790ad40de02ed1b58b9ee00c6c777
Hanna Vasylenko <hanna.vasylenko05@gmail.com>
2022-02-11 17:22:13 +0200

    starts 3d task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,12 @@
+const refs = {
+  form: document.querySelector('.form'),
+  btn: document.querySelector('button'),
+  delay: document.querySelector('.form input[name="delay"]'),
+  step: document.querySelector('.form input[name="step"]'),
+  amount: document.querySelector('.form input[name="amount"]'),
+};
+console.log(refs.btn);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +15,13 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
+refs.btn.addEventListener('submit', createPromise);
