3f9fd87a25b8adf21dcb7e4a704d801021a9cd47
Hanna Vasylenko <hanna.vasylenko05@gmail.com>
2022-02-11 10:39:11 +0200

    part of second task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,58 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+
+const btnStartRef = document.querySelector('button');
+const inputRef = document.querySelector('#datetime-picker');
+//------ФУНКЦИЯ ПОДСЧЕТА ЗНАЧЕНИЙ------//
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+// const refs = {
+//   days: document.querySelector('[data-days]'),
+//   hours: document.querySelector('[data-hours]'),
+//   minutes: document.querySelector('[data-minutes]'),
+//   seconds: document.querySelector('[data-seconds]'),
+// };
+
+let userDate = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    userDate = selectedDates[0].getTime();
+    if (userDate < Date.now()) {
+      alert('Please choose a date in the future');
+      btnStartRef.setAttribute('disabled', 'disable');
+      return;
+    }
+    //Eсли пользователь выбрал дату в прошлом, покажи window.alert() с текстом "Please choose a date in the future".
+    startBtn.removeAttribute('disabled');
+  },
+};
+
+const fp = new flatpickr('#datetime-picker', options);
+//---
+const padNum = num => String(num).padStart(2, 0);
+
+const onTimer = () => {
+  let intervalId = null;
+  intervalId = setInterval(() => {
+    const currentDate = Date.now();
+    const diff = userDate - currentDate;
+    console.log(intervalId);
+  }, 1000);
+};
+btnStartRef.addEventListener('click', onTimer);
